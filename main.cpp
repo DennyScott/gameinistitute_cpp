e5bfa5c47fba53741883b35ddac5f16b72aec392
Denny Scott <dennyscott301@gmail.com>
2014-11-15 16:31:59 -0600

    Slide 4

---
+++ main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  CSeed
+//
+//  Created by Denny Scott on 2014-11-15.
+//  Copyright (c) 2014 Denny Scott. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+
+int main(int argc, const char * argv[]) {
+	std::string firstName = "";
+	std::cout << "Enter you first name and press Enter: ";
+	std::cin >> firstName;
+	std::cout << std::endl;
+    // insert code here...
+    std::cout << "Hello, " << firstName << std::endl <<std::endl;
+}
