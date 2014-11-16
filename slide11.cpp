97c45bdad9566fc4b50b5ed15b9b94f290448946
Denny Scott <dennyscott301@gmail.com>
2014-11-15 19:16:20 -0600

    Slide 11

---
+++ slide11.cpp
@@ -0,0 +1,32 @@
+//
+//  main.cpp
+//  CSeed
+//
+//  Created by Denny Scott on 2014-11-15.
+//  Copyright (c) 2014 Denny Scott. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+	char letter = 'A';
+	int integer = 0;
+	float dec = 0.0f;
+
+	cout << "Enter a letter: ";
+	cin >> letter;
+	cout << "Enter an Integer: ";
+	cin >> integer;
+
+	cout << "Enter a float number: ";
+	cin >> dec;
+
+	cout << endl;
+
+	cout << "Letter: " << letter << endl;
+	cout << "Integer: " << integer << endl;
+	cout << "Float: " << dec << endl;
+
+}
